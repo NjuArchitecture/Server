be1489dc99a9a90aadfdad54b5f365b95ec7ca8d
fw14 <214731923@qq.com>
2017-04-11 23:02:29 +0800

    SensitiveFilter

--- src/main/java/cn/edu/nju/service/filters/commentFilter/SensitiveFilter.java
+++ src/main/java/cn/edu/nju/service/filters/commentFilter/SensitiveFilter.java
@@ -1,19 +1,46 @@
 package cn.edu.nju.service.filters.commentFilter;
 
-import java.util.List;
-import cn.edu.nju.service.pipe.*;
+import cn.edu.nju.service.filters.CommentInfo;
 import cn.edu.nju.service.pipe.IFilter;
-import cn.edu.nju.utility.Comment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class SensitiveFilter implements IFilter<Comment> {
-	
+public class SensitiveFilter implements IFilter<CommentInfo> {
+	static SensitiveWords s=new SensitiveWords();
+	public static String[] sensitiveWords=s.getSensitiveWords();
+
 	@Override
-	public List<Comment> doFilter(List<Comment> list) {
+	public List<CommentInfo> doFilter(List<CommentInfo> list) {
+
 		// TODO Auto-generated method stub
+		List<CommentInfo>result_comment=new ArrayList<CommentInfo>();
+		for(CommentInfo c:list){
+			if(isSensitive(c)==false){
+				result_comment.add(c);
+			}else{
+				System.out.println("Sensitive:"+c.getId());
 
-		
-		return list;
+			}
+
+		}
+		return result_comment;
+
 	}
 
+
+	public static boolean isSensitive(CommentInfo c){
+		String content=c.getContent();
+		boolean sensitive=false;
+		int len=sensitiveWords.length;
+		for(int i=0;i<len;i++){
+			if(content.contains(sensitiveWords[i])){
+				sensitive=true;
+				break;
+			}
+		}
+		return sensitive;
 	}
+
+}
---
+++ src/main/java/cn/edu/nju/service/filters/commentFilter/SensitiveWords.java
@@ -0,0 +1,18 @@
+package cn.edu.nju.service.filters.commentFilter;
+
+public class SensitiveWords {
+
+	private String[] sensitiveWords={"aaaa","bbbb"};
+
+	public String[] getSensitiveWords() {
+		return sensitiveWords;
+	}
+
+	public void setSensitiveWords(String[] sensitiveWords) {
+		this.sensitiveWords = sensitiveWords;
+	}
+	
+	
+	
+	
+}
